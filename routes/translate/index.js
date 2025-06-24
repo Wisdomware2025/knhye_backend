cd925f25cee16449defcf4c2268bc6e5b669023b
Knhye <knahye311@gmail.com>
2025-06-24 16:59:04 +0900

    translate router 수정

--- routes/translate/index.js
+++ routes/translate/index.js
@@ -8,6 +8,6 @@ import authMiddleware from "../../middlewares/auth/index.js"
 const router = Router()
 
 router.post("/", handleTranslation)
-router.post("/cancel", authMiddleware, cancelTranslate)
+router.post("/cancel", cancelTranslate)
 
 export default router
