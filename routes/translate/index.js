53bb396291e54d7c45bbfefe218c3aa4b08a56b4
Knhye <knahye311@gmail.com>
2025-06-24 17:12:26 +0900

    CommentController 수정

--- routes/translate/index.js
+++ routes/translate/index.js
@@ -3,7 +3,6 @@ import {
   cancelTranslate,
 } from "../../controllers/translate/index.js"
 import { Router } from "express"
-import authMiddleware from "../../middlewares/auth/index.js"
 
 const router = Router()
 
