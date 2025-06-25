21ab9f7d0e56d3aad944185d360f68e31456f4ef
Knhye <knahye311@gmail.com>
2025-06-25 20:41:46 +0900

    수정

--- models/translate/Translation.js
+++ models/translate/Translation.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose"
 
 const TranslationSchema = new mongoose.Schema(
   {
-    input: { type: String, required: true },
-    output: { type: String },
+    input: [{ type: String, required: true }], // 문자열 배열로 변경
+    output: [{ type: String }], // 문자열 배열로 변경
   },
   {
     timestamps: true,
